17ee6715e59ebe0665cd04a609e1c9bb73ac4623
steveltomlin <steveltomlin@gmail.com>
2016-06-29 12:45:00 +0100

    dropdown example, excluding css

--- www/assets/js/myreact/dropdown.jsx
+++ www/assets/js/myreact/dropdown.jsx
@@ -1,7 +1,8 @@
 
 require('../../sass/dropdown/_default.scss');
 
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
 
 function each(obj, callback){
 	return (Array.isArray(obj))? forArray(obj, callback):forObject(obj, callback);
@@ -12,7 +13,6 @@ function forArray(obj, callback){
 function forObject(obj, callback){
 	return Object.keys(obj).map(callback);
 }
-
 
 function isFirstKeyString(obj){
 	var strFirstKey = Object.keys(obj)[0];
@@ -25,35 +25,67 @@ class Dropdown extends React.Component {
 
 	constructor /* function */ (props) {
 		super(props);	
+
+		this.state = {
+			selectedValue:this.props.selectedValue,
+			selectedText: this.props.selectedText,
+			ulHidden: true
+		}
 	}
 
-	_onSelectedBoxChange (event){// handle ENTER KEY.....
-		return;
+	_onSelectBoxChange (event){// handle ENTER KEY.....
+		var target = event.nativeEvent.target;
+		var index = target.selectedIndex;
+		var selectedText = target[index].text;
+		var selectedValue =  target[index].value;
+
+		return this.setState({
+			selectedValue: selectedValue,
+			selectedText: selectedText
+		})
 	}
 
 	_onSelectedItemClick (event){// handle ENTER KEY.....
-		return;
+		return this.setState({
+			ulHidden:false
+		})
 	}
 
-	_onLiClick (event){// handle ENTER KEY.....
-		return;
+	_onLiClick (strOptionKey, strOptionText, event){// handle ENTER KEY.....
+
+		//ReactDOM.findDOMNode(this.refs.mySelect).dispatchEvent(new Event('change', { 'bubbles': true }));
+		return this.setState({
+			selectedValue: strOptionKey,
+			selectedText: strOptionText,
+			ulHidden:true
+		})
+
 	}
 
 	render () {    
-		var props = this.props;
-		var options = props.options;
-		var selectBoxId = props.selectBoxId;
-		var selectedValue = props.selectedValue;
+
+		var options = this.props.options;		
+		var ulId = this.props.selectBoxId + '-ul';
+		var pId = this.props.selectBoxId + '-p';
+		var labelId = this.props.selectBoxId + '-label';
+
+		// states
+		var selectedValue = this.state.selectedValue;
+		var selectedText = this.state.selectedText;
+		var ulHidden = this.state.ulHidden; // true || false
+
 		return (
 			<div className="dropdown">
-				<label htmlFor={this.props.selectBoxId}>{this.props.selectBoxLabel}</label>
+				<label htmlFor={this.props.selectBoxId} id={labelId}>{this.props.selectBoxLabel}</label>
 
 				<select 
+					id={this.props.selectBoxId}	
+					role="select"
+					aria-controls={ulId + ' ' + pId}		
+					onChange={this._onSelectBoxChange.bind(this)} 									
 					name={this.props.selectBoxName}
 					className={this.props.selectBoxClassName}
-					id={selectBoxId}
-					onChange={this._onSelectBoxChange} 
-					defaultValue={selectedValue}
+					value={selectedValue}								
 					>
 					{do{
 						if(Array.isArray(options)){							
@@ -77,11 +109,35 @@ class Dropdown extends React.Component {
 
 				</select>
 
-				<p onClick={this._onSelectedItemClick} className="selectedItem">{this.props.selectedText}</p>
+				<p id={pId}
+					role="button"
+					aria-controls={ulId}
+					onClick={this._onSelectedItemClick.bind(this)} 
+					className="selectedItem" 
+					data-selectedValue={selectedValue} 
+					aria-labelledby={labelId}
+					>
+					
+					{selectedText}
+				</p>
 
-				<ul>
-
-
+				<ul id={ulId} 
+					aria-hidden={ulHidden} 
+					className="ulSelect">
+					{
+						each(options, (strOptionKey) => (
+							<li 
+								className="liSelect"								
+								role="option"
+								aria-controls={this.props.selectBoxId + ' ' + pId} key={strOptionKey} 
+								onClick={this._onLiClick.bind(this, strOptionKey, options[strOptionKey])} 
+								aria-labelledby={labelId}								
+								aria-selected={selectedValue === strOptionKey}								
+								>
+								{options[strOptionKey]}
+							</li>
+						))
+					}
 				</ul>
 			</div>
     	)
@@ -102,26 +158,20 @@ Dropdown.propTypes = {
 Dropdown.defaultProps = {
 
 	selectBoxId: 'selectComponent',
+
 	selectBoxLabel:'I am a dropdown',
 	selectBoxName: 'selectComponent',	
 	selectBoxClassName: 'selectBoxDropDown',
-
+	selectedValue:'',
+	selectedText:'Please select',
 	options:{
 		'':'Please select',	
 		'apple':'Lovely green apple',
 		'orange':'juicy orange',
 		'banana':'fat banana'		
 	},
-	/*
-	options:[{
-		'':'Please select',	
-		'apple':'Lovely green apple',
-		'orange':'juicy orange',
-		'banana':'fat banana'
-	}],	
-	*/
-	/*
-	options:[
+	/* OR 
+	optGroups:[
 		{
 			'groupX':{	
 				'':'Please select',	
@@ -139,8 +189,6 @@ Dropdown.defaultProps = {
 			}		
 		}
 	],*/
-	selectedValue:'',
-	selectedText:'Please select'
 }
 
 export default Dropdown;
